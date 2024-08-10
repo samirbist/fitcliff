5f1373f6fd40d04b76454b5e6d129d8b40c7332b
EMEAAD\sbist <samir.bist@soprasteria.com>
2024-08-10 11:25:00 +0530

    This is a new commit for what I originally planned to be amended

---
+++ src/main/java/com/gym/fitcliff/FitcliffApplication.java
@@ -0,0 +1,13 @@
+package com.gym.fitcliff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitcliffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitcliffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gym/fitcliff/FitcliffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.fitcliff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitcliffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
